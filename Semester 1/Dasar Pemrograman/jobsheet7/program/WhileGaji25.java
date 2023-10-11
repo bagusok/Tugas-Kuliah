dac7af6e3f4e76a8e7af5b194f2659e2a49e3bec
Bagusok <bagus.ok55@gmail.com>
2023-10-11 12:45:47 +0700

    add jb 7 perc 2 gajiLembur

---
+++ Semester 1/Dasar Pemrograman/jobsheet7/program/WhileGaji25.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class WhileGaji25 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int jumlahKaryawan, jumlahJamLembur;
+        double gajiLembur = 0, totalGajiLembur = 0;
+        String jabatan;
+
+        System.out.print("Masukkan jumlah karyawan: ");
+        jumlahKaryawan = scan.nextInt();
+
+        int i = 0;
+
+        while (i < jumlahKaryawan) {
+            System.out.print("Pilihan jabatan - Direktur, Manajer, Karyawan");
+            System.out.print("Masukkan jabatan karyawan ke- " + (i + 1) + ": ");
+            jabatan = scan.next();
+
+            System.out.print("Masukkan jumlah jam lembur: ");
+            jumlahJamLembur = scan.nextInt();
+            i++;
+
+            if (jabatan.equalsIgnoreCase("direktur")){
+                continue;
+            }else if (jabatan.equalsIgnoreCase("manajer")){
+                gajiLembur = jumlahJamLembur * 100000;
+            }else if (jabatan.equalsIgnoreCase("karyawan")) {
+                gajiLembur = jumlahJamLembur * 75000;
+            }
+
+            totalGajiLembur += gajiLembur;
+        }
+
+        System.out.print("Total gaji lembur: " + totalGajiLembur );
+    }
+}
