e0bddcd0e6d81f724a3990e1e3b065a95cf74f6a
Bagusok <bagus.ok55@gmail.com>
2023-10-11 12:55:25 +0700

    modify: add invalid jabatan

--- Semester 1/Dasar Pemrograman/jobsheet7/program/WhileGaji25.java
+++ Semester 1/Dasar Pemrograman/jobsheet7/program/WhileGaji25.java
@@ -27,6 +27,10 @@ public class WhileGaji25 {
                 gajiLembur = jumlahJamLembur * 100000;
             }else if (jabatan.equalsIgnoreCase("karyawan")) {
                 gajiLembur = jumlahJamLembur * 75000;
+            }else{
+                i--;
+                System.out.println("Jabatan invalid");
+                continue;
             }
 
             totalGajiLembur += gajiLembur;
