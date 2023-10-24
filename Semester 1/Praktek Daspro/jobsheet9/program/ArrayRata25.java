749a05b7c8dc70421cb7ad78a0d1ab205fd94877
Bagusok <bagus.ok55@gmail.com>
2023-10-24 21:02:29 +0700

    edit jb 9 add percobaan 3

---
+++ Semester 1/Praktek Daspro/jobsheet9/program/ArrayRata25.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class ArrayRata25 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah siswa: ");
+        int jml = sc.nextInt();
+
+        int[] nilaiMhs = new int[jml];
+        double total = 0;
+        int lulus = 0;
+        double sumLulus = 0;
+        double sumTidakLulus = 0;
+
+        for(int i=0; i<nilaiMhs.length; i++) {
+            System.out.print("Masukkan nilai ke-" + (i+1) + ": ");
+            nilaiMhs[i] = sc.nextInt();
+            if (nilaiMhs[i] > 70) {
+                lulus++;
+                sumLulus += nilaiMhs[i];
+            }else{
+                sumTidakLulus += nilaiMhs[i];
+            }
+        }
+
+        for(int i=0; i<nilaiMhs.length; i++) {
+            total += nilaiMhs[i];
+        }
+
+        double rataLulus = sumLulus / lulus;
+        double rataTidakLulus = sumTidakLulus / (nilaiMhs.length - lulus);
+
+        System.out.println("Rata-rata nilai lulus = " + rataLulus);
+        System.out.println("Rata-rata nilai tidak lulus = " + rataTidakLulus);
+
+    
+        sc.close();
+    }
+}
