6227a2637b16f6f484016fc1388b6ab46f3780e4
Bagusok <bagus.ok55@gmail.com>
2023-11-22 12:19:53 +0700

    jb13 add percobaan 1

---
+++ Semester 1/Praktek Daspro/jobsheet13/program/TerimaKasih25.java
@@ -0,0 +1,13 @@
+public class TerimaKasih25 {
+    public static void main(String[] args) {
+        System.out.println("Thank you for being best teacher in the woorld. \n"+
+        "You inspired in me a love for learning and made me feel like I could ask you anything. \n");
+        
+        ucapanTerimaKasih();
+    }
+    public static void ucapanTerimaKasih() {
+        System.out.println("Thank you for being best teacher in the woorld. \n"+
+        "You inspired in me a love for learning and made me feel like I could ask you anything. \n");
+        
+    }
+}
